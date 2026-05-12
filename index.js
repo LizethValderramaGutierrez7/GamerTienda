2583ccf647258aa7bb7dfa47e32f089dd2948160
LizethValderrama <2002liz2002@gmail.com>
2026-05-12 17:35:36 -0600

    Se agrega archivo

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
-let nombre = prompt("¿Cuál es su nombre?");
+/*let nombre = prompt("¿Cuál es su nombre?");
 let productos = prompt("Indiqueme la cantidad de productos en su compra");
-let monto = prompt("Indique el total de su compra");
+let monto = prompt("Indique el total de su compra");*/
 
 import { calcularDescuento } from "./descuentos.js";
-console.log(calcularDescuento);
+console.log(calcularDescuento(totalCompra));
 
 import { totalDescuento } from "./descuentos.js";
-console.log(totalDescuento);
+console.log(totalDescuento(totalFinal));
 
 import { calcularEnvio } from "./envio.js";
-console.log(calcularEnvio);
+console.log(calcularEnvio(cantidadProductos));
 
 import { esVIP } from "./vip.js";
-console.log(esVIP);
+console.log(esVIP(cantidad, total));
 
-console.log(`Hola ${nombre}, la cantidad de tus productos son: ${productos}, la cantidad total de tu compra: ${monto}`);
-console.log(`Costo de envio ${calcularEnvio}`);
-console.log(`Tienes un descuento del ${calcularDescuento}`);
-console.log(`El total de tu compra con el descuento es: ${totalDescuento}`);
+/*console.log(`Hola ${nombre}, la cantidad de tus productos son: ${productos}, la cantidad total de tu compra: ${monto}`);*/
+/*console.log(calcularDescuento(totalCompra));
+console.log(totalDescuento(totalFinal));
+console.log(calcularEnvio(cantidadProductos));
+console.log(esVIP(cantidad, total));*/
